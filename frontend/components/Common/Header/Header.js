b0a4584b042687a248f34a58d4b102b3e3c749ed
Razine Tarik <ra.devweb.io@gmail.com>
2022-04-02 23:26:43 +0000

    search

--- frontend/components/Common/Header/Header.js
+++ frontend/components/Common/Header/Header.js
@@ -2,19 +2,22 @@ import Link from 'next/link';
 
 import Navigation from '../Navigation';
 import { Cart } from '../../UI/CartUI';
+import { Search } from '../../UI';
 
 import { Logo, HeaderStyled } from './Header.styles';
 
 const Header = () => (
   <HeaderStyled>
-    <div className='bar'>
+    <div className="bar">
       <Logo>
-        <Link href='/'>Sick fits</Link>
+        <Link href="/">Sick fits</Link>
       </Logo>
       <Navigation />
       <Cart />
     </div>
-    <div className='sub-bar'>Search</div>
+    <div className="sub-bar">
+      <Search />
+    </div>
   </HeaderStyled>
 );
 
---
+++ frontend/components/UI/Search/Search.js
@@ -0,0 +1,109 @@
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+
+import { resetIdCounter, useCombobox } from 'downshift';
+import debounce from 'lodash.debounce';
+import gql from 'graphql-tag';
+import { useLazyQuery } from '@apollo/client';
+import { DropDown, DropDownItem, SearchStyles } from './Search.styles';
+
+const SEARCH_PRODUCTS_QUERY = gql`
+  query SEARCH_PRODUCTS_QUERY($searchTerm: String!) {
+    searchTerms: products(
+      where: {
+        OR: [
+          { name: { contains: $searchTerm } }
+          { description: { contains: $searchTerm } }
+        ]
+      }
+    ) {
+      id
+      name
+      photo {
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+const Search = () => {
+  const router = useRouter();
+
+  const [findItems, { loading, data, error }] = useLazyQuery(
+    SEARCH_PRODUCTS_QUERY,
+    {
+      fetchPolicy: 'no-cache',
+    }
+  );
+
+  const items = data?.searchTerms || [];
+
+  const findItemsButChill = debounce(findItems, 350);
+
+  resetIdCounter();
+  const {
+    isOpen,
+    inputValue,
+    getMenuProps,
+    getInputProps,
+    getComboboxProps,
+    getItemProps,
+    highlightedIndex,
+  } = useCombobox({
+    items,
+    onInputValueChange() {
+      findItemsButChill({
+        variables: {
+          searchTerm: inputValue,
+        },
+      });
+    },
+    onSelectedItemChange({ selectedItem }) {
+      router.push({
+        pathname: `/product/${selectedItem.id}`,
+      });
+    },
+    itemToString: (item) => item?.name || '',
+  });
+
+  return (
+    <SearchStyles>
+      <div {...getComboboxProps()}>
+        <input
+          {...getInputProps({
+            type: 'search',
+            placeholder: 'Search for an Item',
+            id: 'search',
+            className: loading ? 'loading' : null,
+          })}
+        />
+      </div>
+      <DropDown {...getMenuProps()}>
+        {isOpen &&
+          items.map((item, index) => (
+            <DropDownItem
+              {...getItemProps({ item, index })}
+              key={item.id}
+              highlighted={index === highlightedIndex}
+            >
+              <Image
+                src={item.photo.image.publicUrlTransformed}
+                alt={item.name}
+                objectFit="cover"
+                width={50}
+                height={50}
+              />
+              {item.name}
+            </DropDownItem>
+          ))}
+        {isOpen && !items.length && !loading && (
+          <DropDownItem>Sorry, No items found for {inputValue}</DropDownItem>
+        )}
+      </DropDown>
+    </SearchStyles>
+  );
+};
+
+export default Search;
